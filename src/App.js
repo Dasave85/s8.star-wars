495e466bdb860dbdea04434165bad35104fd5f83
Daniel Sanchez <dasave85@gmail.com>
2023-01-04 17:18:36 +0100

    run

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Navigate to={"/home"} />} />
             <Route path="/home" element={<Home />} />
-            <Route path="*/" element={<Navigate to={"/home"} />} />
 
             <Route
               path="/signup"
